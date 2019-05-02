70d0c6feb4eb1db34ed70efd6f6428c40547e1b6
silesmek <49080226+silesmek@users.noreply.github.com>
2019-05-01 21:51:56 -0600

    Delete Sem3_Ej1_lista_compras.py

--- Sem3_Ej1_lista_compras.py
+++
@@ -1,7 +0,0 @@
-#lista de compras
-lista_de_compras=[]
-verduras=['tomates','papas','cebollas','ajos']
-frutas=['piña','naranja','sandía']
-carnes=['mortadela','pollo','costilla de cerdo']
-limpieza=['jabon','cloro','shampoo']
-pass
