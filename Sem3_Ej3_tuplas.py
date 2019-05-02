0cf89f7eb055f3d0d405aceb4b5f247c331f1aa7
silesmek <49080226+silesmek@users.noreply.github.com>
2019-05-01 21:50:41 -0600

    Delete Sem3_Ej3_tuplas.py

--- Sem3_Ej3_tuplas.py
+++
@@ -1,7 +0,0 @@
-#Crear una tupla
-#frutas=tuple(['mango','uvas','manzana','pera'])
-frutas=('mango','uvas','manzana','pera')
-print(frutas)
-frutas+=('piña',)
-print(frutas[::2])
-print(frutas[::-2])
